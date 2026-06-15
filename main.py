5a44623d7d1884a62c960b39d2f413f7a6c43a5b
Graham Chonghwa Su <gsu@Grahams-MacBook-Air.local>
2026-06-15 11:23:36 -0600

    fixed docker

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app=FastAPI()
 def find_palindrome(x):
     tracked_x=int(str(x)[::-1])
     while tracked_x != x:
-            x=x+tracked_x
+            x=int(x)+tracked_x
             goal=int(str(x)[::-1])
             if goal==x:
                   tracked_x=int(x)
